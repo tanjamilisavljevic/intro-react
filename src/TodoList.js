4c6f78a22c09bb75a61c2e00f7a46fb635c1d6e8
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-08-30 14:16:55 +0200

    Create a task list

---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function TodoList() {
+    return (
+        <ul>
+            <li> <input type="checkbox" /> First task </li>
+            <li> <input type="checkbox" /> Second task </li>
+        </ul>
+    );
+}
+
+
