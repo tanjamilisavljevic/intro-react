aaa772a855c245aaa329f7399ef04e10e75844f5
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-01 11:25:49 +0200

    WIP save new todos to local storage

--- src/Form.js
+++ src/Form.js
@@ -1,20 +1,13 @@
 import React, {useRef, useState, useEffect} from 'react';
 import TodoList from "./TodoList";
 
-const LOCAL_STORAGE_KEY = "MyTodoApp.todos";
+const LOCAL_STORAGE_KEY = "TodoList.todos";
 
 export default function Form() {
     const initialTodos = [
-        {name: 'First task', id: '1', complete: 'false'},
-        {name: 'Second task', id: '1', complete: 'false'}
+        {name: 'First task', id: 1, complete: 'false'},
+        {name: 'Second task', id: 1, complete: 'false'}
     ];
-    const inputRef = useRef();
-    const [todos, setTodos] = useState(initialTodos);
-
-    function clickHandler() {
-        const inputElement = inputRef.current.value;
-        setTodos([...todos, {name: inputElement, id: 1, complete: 'false'}]);
-    }
 
     // useEffect(() => {
     //         const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
@@ -22,19 +15,29 @@ export default function Form() {
     //     }
     // );
 
+    const inputRef = useRef();
+    // const [todos, setTodos] = useState(initialTodos);
+    const [todos, setTodos] = useState(initialTodos.concat(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))));
+    // console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) && initialTodos);
+
+    function clickHandler() {
+        const inputElement = inputRef.current.value;
+        setTodos([...todos, {name: inputElement, id: 1, complete: 'false'}]);
+    }
+
     useEffect(() => {
             window.localStorage.setItem(LOCAL_STORAGE_KEY + ".todos", JSON.stringify(todos));
         }, [todos]
     );
 
-    console.log(todos);
+    // console.log(todos);
     return (
         <>
             <label htmlFor="newTodo"> Add a To-do: </label>
             <input ref={inputRef} type="text" name="newTodo" id="newTodo" placeholder="Do groceries..."/>
 
             <input type="submit" value="submit" id="submit" onClick={clickHandler}/>
-            < TodoList todos={todos}/>
+            <TodoList todos={todos}/>
         </>
     );
 
